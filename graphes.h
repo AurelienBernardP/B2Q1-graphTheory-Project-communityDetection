33aabd5857fe42a230aa786a02b95046eec8d349
Kenan Ozdemir <k.ozdemir@student.uliege.be>
2018-11-24 17:06:55 +0100

    Explications des nouveaux champs

--- graphes.h
+++ graphes.h
@@ -17,22 +17,42 @@ struct edge {
 };
 
 struct node {
-   long unsigned int myCom;
+   //De type Community car quand le gain est supérieur on Peut
+   //Transferer le membre plus rapidement à la communauté de son voisin
+   //(évite de parcourir toutes les communauté jusqu'à trouver la bonne)
+   Community *myCom;
+
+   //Evite de parcourir tous ses arcs pour trouver son poids.
+   //Il suffit de le calculer au moment où on rajoute un arc à ce membre
    double weightNode;
+
    Edge *neighbours;
+
+   //Lorsqu'il faut changer le membre de communauté il suffit d'acceder
+   //au membre précédent de sa communauté courrante et faire pointer
+   //ce membre précédent au suivant
    Node *previous;
    Node *next;
 };
 
 struct community{
    long unsigned int label;
+
+   //Utile de garder le weightInt car il permet de supprimer la Communauté
+   //s'il n'y a plus de membre. Une alternative à ça ce serait de compter
+   //le nombre de membre de la commaunté et permettrait aussi de donner
+   //peut etre des infos plus utiles lors de l'inscription de la nouvelle
+   //matrice.   Pas pue comparer member à null quand on lui enleve son seul
+   //membre. Pourtant je free bien son unique membre mais il ne voit pas
+   //com->member == NULL à méditer...
    double weightInt;
    Node *member;
    Community *next;
 };
 
 struct graph{
-	long unsigned int nbNode;
+	long unsigned int nbCommunity;
+    //NbEdge pas necessaire
 	long unsigned int nbEdge;
 	double weightTot;
 	Community *community;

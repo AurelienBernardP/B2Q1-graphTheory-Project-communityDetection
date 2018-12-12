ab42ae7c52f0c1b285221f96b51dac6e101531b3
Kenan Ozdemir <k.ozdemir@student.uliege.be>
2018-12-12 03:02:24 +0100

    Replace pass.h

--- pass.h
+++ pass.h
@@ -4,23 +4,6 @@
 
 #include "graphes.h"
 
-typedef struct{
-    double weight; 
-    size_t destCommunityLabel;
-}BetweenCommunityEdge;
-
-static double** makeAdjacenceMatrix(Graph* g);
-
-static int printGraphWithCommunities(Graph* g, double** AdjacenceMatrix);// we are maybe going to need the number of the pass to give different file names to the matrix printouts.
-
-static int makeTrace(Graph* g);
-
-static size_t findIndexFromLabel(Graph* g, size_t communityLabel, BetweenCommunityEdge* communityConections);
-
-static void condenseLinksBetweenCommunities(Graph* g, Community* community, BetweenCommunityEdge* communityConections );
-
-static int makeNewGraph(Graph* oldG, Graph* newG);
-
-int pass(Graph* oldG);
+Graph* pass(Graph* oldG);
 
 #endif

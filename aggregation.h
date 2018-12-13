b48dbe2cb51d09340f3a5496382a4d2bddd304ff
Kenan Ozdemir <k.ozdemir@student.uliege.be>
2018-12-13 23:52:50 +0100

    Replace aggregation.h

--- aggregation.h
+++ aggregation.h
@@ -1,9 +1,22 @@
-#if defined(PASS_H)
+#if defined(AGGREGATION_H)
 #else
-#define PASS_H
+#define AGGREGATION_H
 
-#include "graphes.h"
+#include "optimisation.h"
 
-Graph* pass(Graph* oldG,unsigned int passNumber);
+/* communityAggregation
+* Create a new file in $filename$ which keep a trace of the old graph $oldG$
+* and apply communityAggregation (described in the article).
+*
+* [input]
+* oldG : the graph which needs community aggregation
+* filename : the path to a file
+*
+* [return]
+* newG : a new graph which is a condensed version of the graph $oldG$
+* NULL : an error occured
+*
+*/
+Graph* communityAggregation(Graph* oldG,char* filename);
 
 #endif

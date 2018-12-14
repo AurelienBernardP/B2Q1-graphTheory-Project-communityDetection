a4890124db4aac40efe2a9aac47ec93252049d26
Kenan Ozdemir <k.ozdemir@student.uliege.be>
2018-12-14 20:35:06 +0100

    Final

--- aggregation.h
+++ aggregation.h
@@ -5,11 +5,11 @@
 #include "optimisation.h"
 
 /* communityAggregation
-* Create a new file in $filename$ which keep a trace of the old graph $oldG$
-* and apply communityAggregation (described in the article).
+* Create a new file in $filename$ which keeps a trace of a graph $oldG$
+* and create a condensed version of that graph.
 *
 * [input]
-* oldG : the graph which needs community aggregation
+* oldG : the graph which needs to be condense
 * filename : the path to a file
 *
 * [return]
@@ -18,5 +18,19 @@
 *
 */
 Graph* communityAggregation(Graph* oldG,char* filename);
+
+/* makeTrace
+* Saving the graph $g$ in the file $filename$
+*
+* [input]
+* g : an initialised graph
+* filename : the path to a non-existent file
+*
+* [return]
+* 0 : The graph has been successfully saved in a file
+* -1 : Error at applying the operation
+*
+*/
+int makeTrace(Graph* g, char* filename);
 
 #endif
